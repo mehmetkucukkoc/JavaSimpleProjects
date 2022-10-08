d874bbddb34508f482aa9d7f8a69c628a7f188e7
mehmetkucukkoc <mhmtkckkc@gmail.com>
2022-10-08 14:04:24 +0300

    evenNumbers

---
+++ src/ForLoopStars/EvenNumbersB0and100.java
@@ -0,0 +1,13 @@
+package ForLoopStars;
+
+public class EvenNumbersB0and100 {
+
+    public static void main(String[] args) {
+
+        //    Write code that prints even numbers between 0 and 100, including 0 and 100.
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
---
+++ src/ForLoopStars/Odev08.java
@@ -0,0 +1,14 @@
+package ForLoopStars;
+
+public class Odev08 {
+
+    public static void main(String[] args) {
+
+        /*100'den 0'a kadar bütün tek sayıları yazdırınız.
+        99-98-97-96....
+        100 ve 0 dahil değildir.*/
+
+
+
+    }
+}
