cd041c03faab6ce99cb22bde89b8e40a7bdbec47
mehmetkucukkoc <mhmtkckkc@gmail.com>
2022-10-11 13:14:15 +0300

    LargestPalindromeProduct

--- src/Projects/Deneme.java
+++ src/Projects/Deneme.java
@@ -1,14 +1,28 @@
 package Projects;
 
+import java.util.Scanner;
+
 public class Deneme {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lütfen Bir İfade Giriniz : ");
+        String ifade = sc.nextLine();
+        int aSayisi = 0;
 
-      String str1="www";
-      String str2="google";
-      String str3="com";
-      String str4=str1.concat(".").concat(str2).concat(".").concat(str3);
-        System.out.println(str1.hashCode());
+        for (int i = 0; i < ifade.length(); i++) {
+            if (ifade.charAt(i) == 'a') {
+                aSayisi++;
+
+
+            }
+            if (ifade.charAt(i) == 'c') {
+
+
+                break;
+            }
+
+        } System.out.println(aSayisi);
 
 
     }
