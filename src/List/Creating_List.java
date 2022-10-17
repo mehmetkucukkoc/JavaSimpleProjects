b9750b7fd13fa1249699da54568b129c2dcab2b1
mehmetkucukkoc <mhmtkckkc@gmail.com>
2022-10-17 10:15:43 +0300

    17.10.2022

---
+++ src/List/Creating_List.java
@@ -0,0 +1,12 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Creating_List {
+    public static void main(String[] args) {
+        List<Integer> listSayi = new ArrayList<>();
+        listSayi.add(10);
+        System.out.println(listSayi);
+    }
+}
